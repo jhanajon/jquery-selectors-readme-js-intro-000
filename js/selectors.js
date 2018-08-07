d6f3d5d187b629afdf57995daee8e1ae3502d64e
joe fisher <jhanajon@gmail.com>
2018-08-07 01:06:19 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+$('p');
+return 'p';
+}
+
+
+function lastImageSelector() {
+  $('img:last');
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja');
+}
+
+function divSelector() {
+  $('.pics')
+}
+  
+function firstListItem() {
+  $('UL li #pic-list:first-child')
+}
